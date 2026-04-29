fb61c28746f332e2d70b38dd521fdd1011c14b7f
Anton Sviridov <keynmol@gmail.com>
2026-04-29 15:30:22 +0100

    A fluent example that runs!

---
+++ examples/src/main/scala/raw.scala
@@ -0,0 +1,98 @@
+import sn.gnome.gio.internal.*
+import sn.gnome.glib.internal.*
+import sn.gnome.gtk4.internal.*
+import sn.gnome.gtk4.fluent.*
+import scalanative.unsafe.*
+
+class RawGtk:
+  def run() =
+    gtk_init()
+
+    import Extensions.*
+
+    val callback = CFuncPtr2.fromScalaFunction {
+      (application: Ptr[GtkApplication], data: gpointer) =>
+        val window = gtk_application_window_new(application)
+
+        gtk_window_set_title(
+          window.asInstanceOf[Ptr[GtkWindow]],
+          c"Hello from Scala Native 0.5"
+        )
+        gtk_window_set_default_size(
+          window.asInstanceOf[Ptr[GtkWindow]],
+          200,
+          200
+        )
+
+        val box = gtk_box_new(GtkOrientation.GTK_ORIENTATION_VERTICAL, 0)
+        gtk_widget_set_halign(box, GtkAlign.GTK_ALIGN_CENTER)
+        gtk_widget_set_valign(box, GtkAlign.GTK_ALIGN_CENTER)
+
+        gtk_window_set_child(window.asInstanceOf[Ptr[GtkWindow]], box)
+
+        val button = gtk_button_new_with_label(c"Press me")
+
+        val printHello = CFuncPtr2.fromScalaFunction {
+          (widget: Ptr[GtkWidget], data: gpointer) =>
+            g_print(
+              c"Click! I come from the 0.5 version of Gtk examples\n".asGString
+            )
+        }
+
+        g_signal_connect(button, c"clicked", printHello)
+
+        gtk_box_append(box.asInstanceOf[Ptr[GtkBox]], button)
+
+        gtk_widget_show(window)
+    }
+
+    val app = gtk_application_new(
+      c"org.gtk.example",
+      GApplicationFlags.G_APPLICATION_FLAGS_NONE
+    )
+
+    g_signal_connect(
+      app,
+      c"activate",
+      callback
+    )
+
+    g_application_run(app.asPtr[GApplication], 0, null)
+  end run
+end RawGtk
+
+private object Extensions:
+  import scalanative.unsafe.*
+  import sn.gnome.gobject.internal.*
+  import sn.gnome.glib.internal.*
+  import sn.gnome.gobject.internal.GConnectFlags
+  import sn.gnome.gobject.internal.g_signal_connect_data
+
+  extension [T <: CFuncPtr](inline ptr: T)
+    inline def asGCallback: GCallback =
+      GCallback(
+        CFuncPtr.fromPtr[CFuncPtr0[Unit]](CFuncPtr.toPtr(ptr))
+      )
+
+  extension (cstr: CString)
+    inline def asGString: Ptr[gchar] = cstr.asInstanceOf[Ptr[gchar]]
+
+  extension [T](ptr: Ptr[T])
+    inline def asGpointer: gpointer = gpointer(ptr.asInstanceOf[Ptr[Byte]])
+    inline def asPtr[T1]: Ptr[T1] = ptr.asInstanceOf[Ptr[T1]]
+
+  inline def g_signal_connect[A: Tag, T <: CFuncPtr](
+      instance: Ptr[A],
+      detailed_signal: CString,
+      c_handler: T,
+      data: Ptr[Byte] = null,
+      flags: GConnectFlags = GConnectFlags.define(0L)
+  ): gulong =
+    g_signal_connect_data(
+      instance.asGpointer,
+      detailed_signal.asGString,
+      c_handler.asGCallback,
+      data.asGpointer,
+      null.asInstanceOf[GClosureNotify],
+      flags
+    )
--- gtk4/src/main/scala/fluent.scala
+++ gtk4/src/main/scala/fluent.scala
@@ -1,37 +1,93 @@
-package sn.gnome.gtk4
-package fluent
-
-import scalanative.unsafe.*
-import sn.gnome.gobject.internal.*
+package sn.gnome.gtk4.fluent
+import sn.gnome.gtk4.fluent.*
+import sn.gnome.gtk4.internal.*
+import sn.gnome.gio.internal.*
+import sn.gnome.gio.fluent.Application as GioApplication
 import sn.gnome.glib.internal.*
-import sn.gnome.gobject.internal.GConnectFlags
-import sn.gnome.gobject.internal.g_signal_connect_data
+import sn.gnome.gobject.internal.*
+import scalanative.unsafe.*
+
+import Captures.*
+
+extension [T <: sn.gnome.gobject.fluent.Object](app: T)
+  def connectSignal(
+      signal: String,
+      flags: GConnectFlags = GConnectFlags.G_CONNECT_DEFAULT
+  )(f: T => Unit)(using Zone) =
+    val c_handler = CFuncPtr2.fromScalaFunction {
+      (ptr: Ptr[Byte], data: Ptr[SignalRegistration[T]]) =>
+        val sr = !data
+
+        sr.handler(sr.ref)
+    }
+
+    val sr = SignalRegistration(app, f)
+    val (ptr, mem) = Captured.unsafe(sr)
+
+    import scalanative.runtime.*
+
+    g_signal_connect_data(
+      gpointer(app.getUnsafeRawPointer().asInstanceOf[Ptr[Byte]]),
+      toCString(signal).asInstanceOf[Ptr[gchar]],
+      c_handler.asGCallback,
+      gpointer(ptr.asInstanceOf[Ptr[Byte]]), // data
+      null.asInstanceOf[GClosureNotify],
+      flags
+    )
+end extension
 
 extension [T <: CFuncPtr](inline ptr: T)
-  inline def asGCallback: GCallback =
+  private[fluent] inline def asGCallback: GCallback =
     GCallback(
       CFuncPtr.fromPtr[CFuncPtr0[Unit]](CFuncPtr.toPtr(ptr))
     )
 
-extension (cstr: CString)
-  inline def asGString: Ptr[gchar] = cstr.asInstanceOf[Ptr[gchar]]
-
-extension [T](ptr: Ptr[T])
-  inline def asGpointer: gpointer = gpointer(ptr.asInstanceOf[Ptr[Byte]])
-  inline def asPtr[T1]: Ptr[T1] = ptr.asInstanceOf[Ptr[T1]]
-
-inline def g_signal_connect[A: Tag, T <: CFuncPtr](
-    instance: Ptr[A],
-    detailed_signal: CString,
-    c_handler: T,
-    data: Ptr[Byte] = null,
-    flags: GConnectFlags = GConnectFlags.define(0L)
-): gulong =
-  g_signal_connect_data(
-    instance.asGpointer,
-    detailed_signal.asGString,
-    c_handler.asGCallback,
-    data.asGpointer,
-    null.asInstanceOf[GClosureNotify],
-    flags
+private[fluent] object Captures:
+  case class SignalRegistration[T](
+      ref: T,
+      handler: T => Unit
   )
+  val signalRegistrations =
+    collection.mutable.Map.empty[Int, SignalRegistration[?]]
+
+  val signalZone = Zone.open()
+
+  def registerSignal[T](sr: SignalRegistration[T]) =
+    signalRegistrations.synchronized:
+      val id = signalRegistrations.size
+      signalRegistrations(id) = sr
+      id
+end Captures
+
+private[fluent] object GCRoots:
+  private val references = new java.util.IdentityHashMap[Object, Unit]
+  def addRoot(o: Object): Unit = references.put(o, ())
+  def removeRoot(o: Object): Unit = references.remove(o)
+
+private[fluent] object Captured:
+  def unsafe[D <: AnyRef: Tag](value: D): (Ptr[D], Memory) =
+    import scalanative.runtime.*, ffi.*
+
+    val rawptr = malloc(sizeof[D])
+    val mem = fromRawPtr[D](rawptr)
+    val deallocate: Memory =
+      () =>
+        GCRoots.removeRoot(value.asInstanceOf[Object])
+        free(toRawPtr[D](mem))
+
+    val originalAddress = Intrinsics.castObjectToRawPtr(value)
+
+    Intrinsics.storeObject(rawptr, value)
+
+    GCRoots.addRoot(value)
+
+    (mem, deallocate)
+  end unsafe
+
+  opaque type Memory = () => Unit
+  object Memory:
+    extension (f: Memory)
+      def deallocate() =
+        f()
+
+end Captured
